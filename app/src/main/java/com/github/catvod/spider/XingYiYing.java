0d5de950af0f89f1ce7f24637b96ae17ad5844d9
laoZ <71415980+xyzjhe@users.noreply.github.com>
2024-04-07 12:21:23 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/XingYiYing.java
+++ app/src/main/java/com/github/catvod/spider/XingYiYing.java
@@ -1,7 +1,5 @@
 package com.github.catvod.spider;
 
-import android.text.TextUtils;
-
 import com.github.catvod.crawler.Spider;
 //import com.github.catvod.net.OkHttp;
 import com.github.catvod.utils.okhttp.OkHttpUtil;
@@ -11,12 +9,9 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.net.URLEncoder;
-
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -27,11 +22,11 @@ import java.util.regex.Pattern;
 
 /**
  * @author zhixc
- * 动漫84（动漫巴士）
+ * 星易影
  */
-public class Dm84 extends Spider {
+public class XingYiYing extends Spider {
 
-    private final String siteUrl = "https://dm84.tv";
+    private final String siteUrl = "https://www.xingyiying.com";
 
     private final String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:102.0) Gecko/20100101 Firefox/102.0";
 
@@ -42,16 +37,16 @@ public class Dm84 extends Spider {
         return header;
     }
 
-    private String req(String url) {
-        //return OkHttp.string(url, getHeader());
-        return OkHttpUtil.string(url, getHeader());
+    private Map<String, String> getHeaderForPlay() {
+        Map<String, String> header = new HashMap<>();
+        header.put("Accept", "*/*");
+        header.put("User-Agent", userAgent);
+        return header;
     }
 
-    private String find(String regexStr, String htmlStr) {
-        Pattern pattern = Pattern.compile(regexStr, Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(htmlStr);
-        if (matcher.find()) return matcher.group(1).trim();
-        return "";
+    private String req(String url) {
+//        return OkHttp.string(url, getHeader());
+        return OkHttpUtil.string(url, getHeader());
     }
 
     private JSONArray parseVodList(String url) throws Exception {
@@ -75,7 +70,18 @@ public class Dm84 extends Spider {
         return videos;
     }
 
-    @Override
+    private String find(Pattern pattern, String html) {
+        Matcher m = pattern.matcher(html);
+        return m.find() ? m.group(1).trim() : "";
+    }
+
+    private String parseVodInfo(Element element) {
+        StringBuilder sb = new StringBuilder();
+        for (Element a : element.select("a")) sb.append(a.text()).append(" / ");
+        return sb.toString();
+    }
+
+    /*@Override
     public String homeContent(boolean filter) throws Exception {
         JSONArray classes = new JSONArray();
         List<String> typeIds = Arrays.asList("1", "2", "3", "4");
@@ -92,9 +98,9 @@ public class Dm84 extends Spider {
         result.put("class", classes);
         result.put("filters", filterConfig);
         return result.toString();
-    }
+    }*/
 
-    @Override
+    /*@Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
         // 筛选处理 start
         String year = extend.get("year") == null ? "" : extend.get("year");
@@ -118,43 +124,53 @@ public class Dm84 extends Spider {
         result.put("total", total);
         result.put("list", videos);
         return result.toString();
-    }
+    }*/
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
         String vodId = ids.get(0);
-        String detailUrl = siteUrl + vodId;
+        // https://www.xingyiying.com/index.php/vod/detail/id/217717.html
+        // https://www.xingyiying.com/index.php/vod/detail/id/183491.html
+        // https://www.xingyiying.com/index.php/vod/detail/id/31606.html
+        String detailUrl = siteUrl + "/index.php/vod/detail/id/" + vodId + ".html";
         String html = req(detailUrl);
         Document doc = Jsoup.parse(html);
-        String name = doc.select(".v_title > a").text();
-        String pic = doc.select("#v_content > .cover > img").attr("src");
-        String typeName = doc.select(".v_desc a").text();
-        List<TextNode> textNodes = doc.select(".v_desc").textNodes();
+        String name = doc.select("h1").text();
+        String pic = doc.select(".module-info-poster img").attr("data-original");
+        String typeName = doc.select(".module-info-tag-link").text();
         String year = "";
         String area = "";
-        if (textNodes.size() >= 2) {
-            year = textNodes.get(0).text();
-            area = textNodes.get(1).text();
-        }
-        String remark = doc.select(".v_desc .desc").text();
-        String director = find("导演：(.*?)</p>", html);
-        String actor = find("主演：(.*?)</p>", html);
-        String description = find("剧情：(.*?)</p>", html);
 
-        Elements sourceList = doc.select(".play_list");
-        Elements circuits = doc.select("[class=tab_control play_from] li");
+        String actor = "";
+        String director = "";
+        String remark = "";
+        Elements elements = doc.select(".module-info-items > .module-info-item");
+        for (Element element : elements) {
+            String text = element.text();
+            if (text.startsWith("导演")) director = parseVodInfo(element);
+            if (text.startsWith("主演")) actor = parseVodInfo(element);
+            if (text.startsWith("集数")) remark = text;
+            if (text.startsWith("备注")) remark = text.replaceAll("备注：", "");
+        }
+        String description = doc.select(".module-info-introduction").text();
+
+        Elements circuits = doc.select("#y-playList > .tab-item");
+        Elements sourceList = doc.select("#panel1");
         Map<String, String> playMap = new LinkedHashMap<>();
         for (int i = 0; i < sourceList.size(); i++) {
-            String circuitName = circuits.get(i).text();
+            String spanText = circuits.get(i).select("span").text();
+            String smallText = circuits.get(i).select("small").text();
+            String circuitName = spanText + "【共" + smallText + "集】";
             List<String> vodItems = new ArrayList<>();
             Elements aList = sourceList.get(i).select("a");
             for (Element a : aList) {
+                // https://www.xingyiying.com/index.php/vod/play/id/217405/sid/1/nid/1.html
                 String episodeUrl = siteUrl + a.attr("href");
-                String episodeName = "第" + a.text() + "集";
+                String episodeName = a.text();
                 vodItems.add(episodeName + "$" + episodeUrl);
             }
             if (vodItems.size() > 0) {
-                playMap.put(circuitName, TextUtils.join("#", vodItems));
+                playMap.put(circuitName, String.join("#", vodItems));
             }
         }
 
@@ -170,8 +186,8 @@ public class Dm84 extends Spider {
         vod.put("vod_director", director); // 导演 选填
         vod.put("vod_content", description); // 简介 选填
         if (playMap.size() > 0) {
-            vod.put("vod_play_from", TextUtils.join("$$$", playMap.keySet()));
-            vod.put("vod_play_url", TextUtils.join("$$$", playMap.values()));
+            vod.put("vod_play_from", String.join("$$$", playMap.keySet()));
+            vod.put("vod_play_url", String.join("$$$", playMap.values()));
         }
         JSONArray jsonArray = new JSONArray().put(vod);
         JSONObject result = new JSONObject().put("list", jsonArray);
@@ -185,10 +201,28 @@ public class Dm84 extends Spider {
 
     @Override
     public String searchContent(String key, boolean quick, String pg) throws Exception {
-        String keyWord = URLEncoder.encode(key);
-        String searchUrl = siteUrl + "/s----------.html?wd=" + keyWord;
-        if (!pg.equals("1")) searchUrl = siteUrl + "/s-" + keyWord + "---------" + pg + ".html";
-        JSONArray videos = parseVodList(searchUrl);
+        String keyword = URLEncoder.encode(key);
+        // https://www.xingyiying.com/index.php/ajax/suggest?mid=1&wd=斗破
+        String searchUrl = siteUrl + "/index.php/ajax/suggest?mid=1&wd=" + keyword;
+//        if (!pg.equals("1")) searchUrl = siteUrl + "/s-" + keyword + "---------" + pg + ".html";
+        if (!pg.equals("1")) return "";
+        JSONArray videos = new JSONArray();
+        JSONObject searchResult = new JSONObject(req(searchUrl));
+        JSONArray items = searchResult.optJSONArray("list");
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject item = items.getJSONObject(i);
+            String vodId = item.optString("id");
+            String name = item.optString("name");
+            String pic = item.optString("pic");
+            String remark = "";
+
+            JSONObject vod = new JSONObject();
+            vod.put("vod_id", vodId);
+            vod.put("vod_name", name);
+            vod.put("vod_pic", pic);
+            vod.put("vod_remarks", remark);
+            videos.put(vod);
+        }
         JSONObject result = new JSONObject();
         result.put("list", videos);
         return result.toString();
@@ -197,12 +231,21 @@ public class Dm84 extends Spider {
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) throws Exception {
         String lastUrl = id;
+        int parse = 1;
+        String headerStr = getHeader().toString();
         String html = req(lastUrl);
-        lastUrl = Jsoup.parse(html).select("iframe").attr("src");
+        String player_aaaa = find(Pattern.compile("player_aaaa=(.*?)</script>"), html);
+        JSONObject jsonObject = new JSONObject(player_aaaa);
+        String url = jsonObject.optString("url");
+        if (url.contains(".m3u8") || url.contains(".mp4")) {
+            lastUrl = url;
+            parse = 0;
+            headerStr = getHeaderForPlay().toString();
+        }
 
         JSONObject result = new JSONObject();
-        result.put("parse", 1);
-        result.put("header", getHeader().toString());
+        result.put("parse", parse);
+        result.put("header", headerStr);
         result.put("playUrl", "");
         result.put("url", lastUrl);
         return result.toString();
